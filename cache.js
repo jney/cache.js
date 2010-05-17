238c41631d4e29e27288a62f1f084d684da29fd8
NEY Jean-Sébastien <jeansebastien.ney@gmail.com>
2010-05-17 15:33:27 +0200

    first commit

---
+++ cache.js
@@ -0,0 +1,33 @@
+/**
+ * cache.js
+ * ========
+ * 
+ * cache.js is a javascript prototype function that caches the return of a 
+ * function + args.
+ * 
+ * !!! It doesn't do anything else than storing the return of a function 
+ * in a hash
+ * 
+ * Example :
+ * var test = function (x) {
+ *  return x;
+ * }
+ *
+ * test.cache(1); // execute test(1) function a first time
+ * test.cache(1); // return the content of cache
+ * test.cache(2); // execute test(2) another time because arguments 
+ *                // have changed
+ * 
+ */
+ 
+Function.prototype.cache = function () {
+  if (typeof this.cachedArguments == "undefined") {
+    this.cachedArguments = [];
+  }
+  
+  if (!(arguments in this.cachedArguments)) {
+    this.cachedArguments[arguments] = this.call(this, arguments);
+  }
+  
+  return this.cachedArguments[arguments];
+}
